c7e3c38e34b4a4ca473d3458622b2cf4089c486f
starymalpiszonie <lukasz.ciszak@op.pl>
2014-04-11 15:23:53 +0200

    The solution to the peer assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## The function create the "special" matrix with an associated cache for inversion
+## it uses the generalized matrix inversion function and requieres MASS library
+## this is due to the fact that "solve" only works with square matrices
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(ginv) m <<- ginv
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## the function either calculates the matrix inversion, or returns the cached inversion
+## if it already has been calculated
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+		# if the inverse has already been calculated, return the cached version
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+		# otherwise calculate the inversion, store it in the cache and return the result
+        data <- x$get()
+        m <- ginv(data, ...)
+        x$setsolve(m)
+        m
 }
